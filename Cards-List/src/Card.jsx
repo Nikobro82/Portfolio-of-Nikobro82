b553b1422c7c64df6ee6c1701cdbc39adf957439
Nikobro82 <jeffreyniko19@gmail.com>
2026-02-17 15:07:16 -0500

    First commit!

---
+++ Cards-List/src/Card.jsx
@@ -0,0 +1,48 @@
+import profilepic from "./assets/nikobro82pfp.jpg"
+import destiny2 from "./assets/destiny2.png"
+import {motion, AnimatePresence} from "framer-motion"
+import {useState} from 'react'
+
+function Card(props) {
+    if (!props.table) {return}
+
+    const table = props.table
+
+    const [description, setDescription] = useState(table.desc);
+
+    const desc1 = table.desc
+    const desc2 = table.desc2
+    let descriptionNumber = 1
+
+    const updateDescription = (descUsing) => {
+        setDescription(descUsing)
+    }
+
+    const profile_pictures = {
+        "Nikobro82" : profilepic,
+        "Destiny 2" : destiny2,
+        "default" : profilepic,
+    }
+
+    let has_profile_pic = profile_pictures[table.name]
+    has_profile_pic = has_profile_pic || profile_pictures["default"]
+
+    return(
+        <motion.div className = "card" 
+        initial = {{scale: 0}} 
+        animate = {{scale: 1}} 
+        transition = {{duration: 1, type: 'spring'}}
+        whileHover = {{scale:1.05}}
+        onMouseEnter = {() => updateDescription(desc2)}
+        onMouseLeave = {() => updateDescription(desc1)}>
+            <img src = {has_profile_pic} className = "card-image"></img>
+            <h2 className = "card-title">{table.name}</h2>
+
+            {table.TimeLength ? <p className = "timeSpent-text">Time Spent: {table.TimeLength}</p> : null}
+
+            <p className = "card-text">{description}</p>
+        </motion.div>
+    );
+}
+
+export default Card;
