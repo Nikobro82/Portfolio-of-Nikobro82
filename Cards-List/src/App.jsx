cd07a16bec8840ab7165c57df54855e7f42cdd33
Nikobro82 <jeffreyniko19@gmail.com>
2026-02-18 20:03:01 -0500

    second commit!

--- Cards-List/src/App.jsx
+++ Cards-List/src/App.jsx
@@ -4,7 +4,7 @@ import React from "react"
 function App() {
   const descriptions = {
     "Nikobro82" : {
-      "desc" : "I study Computer Science at NYU and like to play video games.",
+      "desc" : "Hi! I am Niko. I am a high school senior and study Computer Science, aiming to be a future Game Developer.",
       "desc2" : "My favorite game is Destiny 2",
       "imgsrc" : "./assets/nikobro82pfp.jpg",
       "name" : "Nikobro82"
@@ -32,7 +32,7 @@ function App() {
     },
     "Brickbattle Defense" : {
       "desc" : "Brickbattle Defense is a tower defense game that I've worked on the longest. I developed the systems and code for Brickbattle Defense",
-      "desc2" : "lol c00lkidd vs 1x1x1x1, strongest today vs strongest in history",
+      "desc2" : "https://www.roblox.com/home",
       "imgsrc" : "./assets/nikobro82pfp.jpg",
       "TimeLength" : "1 Year and 1/2",
       "name" : "Brickbattle Defense"
@@ -44,22 +44,49 @@ function App() {
       "TimeLength" : "2 Months",
       "name" : "Brickbattle RPG"
     },
+    "Countries Tower Defense" : {
+      "desc" : "My first true video game project, this is where I learned simple modular systems, abstraction with module-scripts and much more.",
+      "desc2" : "lol spaghetti code now!",
+      "imgsrc" : "./assets/nikobro82pfp.jpg",
+      "TimeLength" : "1 Year",
+      "name" : "Countries Tower Defense"
+    },
+    "Portfolio" : {
+      "desc" : "This project is my first serious react/js project! Hope you like it!",
+      "desc2" : "is HTML a programming language?",
+      "imgsrc" : "./assets/nikobro82pfp.jpg",
+      "TimeLength" : "<1 week",
+      "name" : "This Portfolio"
+    },
+
   }
   return (
     <>
       <h1 className = "header">Nikobro82</h1>
       <hr></hr>
-      <Card table = {descriptions["Nikobro82"]}></Card>
-      <Card table = {descriptions["Computer Science"]}></Card>
-      <Card table = {descriptions["History"]}></Card>
-      <hr></hr>
-      <h2 className = "header">Projects</h2>
-      <hr></hr>
-      <Card table = {descriptions["Brickbattle Defense"]}></Card>
-      <Card table = {descriptions["Brickbattle RPG"]}></Card>
-      <h2 className = "header">Games</h2>
-      <hr></hr>
-      <Card table = {descriptions["Destiny 2"]}></Card>
+      <div className = "card-container">
+        <h2 className = "header">Studies</h2>
+        <hr></hr>
+        <Card table = {descriptions["Nikobro82"]}></Card>
+        <Card table = {descriptions["Computer Science"]}></Card>
+        <Card table = {descriptions["History"]}></Card>
+        <hr></hr>
+      </div>
+      <div className = "card-container">
+        <h2 className = "header">Projects</h2>
+        <hr></hr>
+        <Card table = {descriptions["Countries Tower Defense"]}></Card>
+        <Card table = {descriptions["Brickbattle Defense"]}></Card>
+        <Card table = {descriptions["Brickbattle RPG"]}></Card>
+        <Card table = {descriptions["Portfolio"]}></Card>
+        <hr></hr>
+      </div>
+      <div className = "card-container">
+        <h2 className = "header">Games</h2>
+        <hr></hr>
+        <Card table = {descriptions["Destiny 2"]}></Card>
+      </div>
+      
 
     </>
     
--- Cards-List/src/Card.jsx
+++ Cards-List/src/Card.jsx
@@ -29,10 +29,10 @@ function Card(props) {
 
     return(
         <motion.div className = "card" 
-        initial = {{scale: 0}} 
-        animate = {{scale: 1}} 
-        transition = {{duration: 1, type: 'spring'}}
-        whileHover = {{scale:1.05}}
+        initial = {{rotate: "180deg"}} 
+        animate = {{rotate: "0deg"}} 
+        transition = {{duration: 2.25, type: 'spring'}}
+        whileHover = {{scale:1.05, rotate: "360deg"}}
         onMouseEnter = {() => updateDescription(desc2)}
         onMouseLeave = {() => updateDescription(desc1)}>
             <img src = {has_profile_pic} className = "card-image"></img>
